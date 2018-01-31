73d847fc891748ef7b9c58123ff777f4506c86d4
James E. Petts <jamespetts@yahoo.com>
2018-01-31 02:00:15 +0000

    CHANGE: Attempt at increasing the number of players to 32. This currently does not work properly so far as player colours are concerned.

--- simhalt.cc
+++ simhalt.cc
@@ -3776,6 +3776,11 @@ void haltestelle_t::rdwr(loadsave_t *file)
 	}
 	file->rdwr_long(owner_n);
 
+	if (file->get_extended_version() < 14 && owner_n == OLD_PLAYER_UNOWNED && file->is_loading())
+	{
+		owner_n = PLAYER_UNOWNED;
+	}
+
 	if(file->get_version()<=88005) {
 		bool dummy;
 		file->rdwr_bool(dummy); // pax
@@ -3785,6 +3790,11 @@ void haltestelle_t::rdwr(loadsave_t *file)
 
 	if(file->is_loading()) {
 		owner = welt->get_player(owner_n);
+
+		if (file->get_extended_version() < 14 && owner_n == OLD_PLAYER_UNOWNED && file->is_loading())
+		{
+			owner_n = PLAYER_UNOWNED;
+		}
 		k.rdwr( file );
 		while(k!=koord3d::invalid) {
 			grund_t *gr = welt->lookup(k);
