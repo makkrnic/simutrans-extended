8b0aa253c6dbb83651f7ec3d3e9d7001c19a2828
Phystam <phystam.tc@gmail.com>
2020-01-29 17:41:17 +0900

    Use default way to initialise way builder (prissi, from standard)
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@7572 8aca7d54-2c30-db11-9de9-000461428c89

--- simtool.cc
+++ simtool.cc
@@ -2420,7 +2420,14 @@ const way_desc_t *tool_build_way_t::get_desc( uint16 timeline_year_month, bool r
 		desc = defaults[wt & 63];
 		if(desc == NULL || !desc->is_available(timeline_year_month)) {
 			// Search for default way
-			desc = way_builder_t::weg_search(wt, 0xffffffff, timeline_year_month, type_flat);
+			if(  wt == tram_wt  ) {
+				desc = way_builder_t::weg_search(wt, 0xffffffff, timeline_year_month, type_flat);
+			}
+			else {
+				weg_t *w = weg_t::alloc(wt);
+				desc = w->get_desc();
+				delete w;
+			}
 		}
 	}
 	if( desc && remember ) {
