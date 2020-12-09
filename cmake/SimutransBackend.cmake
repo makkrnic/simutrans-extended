fabc914e73fd535554284f7ee91e61f408dfd374
Mak Krnic <mkrnic@gmail.com>
2020-12-09 01:33:09 +0100

    Improve cmake used for compiling vulkan renderer

--- cmake/SimutransBackend.cmake
+++ cmake/SimutransBackend.cmake
@@ -3,32 +3,29 @@
 # (see LICENSE.txt)
 #
 
-if(FORCE_VULKAN_BACKEND)
-    # TODO MAK
-    find_package(SDL2   REQUIRED)
-    find_package(Vulkan REQUIRED)
-    set(SIMUTRANS_BACKEND "sdl2_vulkan" CACHE STRING "Graphics backend")
-else()
-    find_package(SDL2       MODULE)
-    find_package(SDL2_mixer MODULE)
+find_package(SDL2       MODULE)
+find_package(SDL2_mixer MODULE)
 
-    if (SDL2_FOUND)
-        list(APPEND AVAILABLE_BACKENDS "sdl2")
-        mark_as_advanced(SDL2_DIR)
+if (SDL2_FOUND)
+    if (VULKAN_RENDERER)
+        find_package(Vulkan REQUIRED)
+        list(APPEND AVAILABLE_BACKENDS "sdl2_vulkan")
+    endif()
 
-        if (SDL2_mixer_FOUND)
-            list(APPEND AVAILABLE_BACKENDS "mixer_sdl2")
-        endif (SDL2_mixer_FOUND)
-    endif (SDL2_FOUND)
+    list(APPEND AVAILABLE_BACKENDS "sdl2")
+    mark_as_advanced(SDL2_DIR)
 
-    if (WIN32)
-        list(APPEND AVAILABLE_BACKENDS "gdi")
-    endif ()
+    if (SDL2_mixer_FOUND)
+        list(APPEND AVAILABLE_BACKENDS "mixer_sdl2")
+    endif (SDL2_mixer_FOUND)
+endif (SDL2_FOUND)
 
-    list(APPEND AVAILABLE_BACKENDS "none")
+if (WIN32)
+    list(APPEND AVAILABLE_BACKENDS "gdi")
+endif ()
 
-    string(REGEX MATCH "^[^;][^;]*" FIRST_BACKEND "${AVAILABLE_BACKENDS}")
-    set(SIMUTRANS_BACKEND "${FIRST_BACKEND}" CACHE STRING "Graphics backend")
-    set_property(CACHE SIMUTRANS_BACKEND PROPERTY STRINGS ${AVAILABLE_BACKENDS})
+list(APPEND AVAILABLE_BACKENDS "none")
 
-endif(FORCE_VULKAN_BACKEND)
+string(REGEX MATCH "^[^;][^;]*" FIRST_BACKEND "${AVAILABLE_BACKENDS}")
+set(SIMUTRANS_BACKEND "${FIRST_BACKEND}" CACHE STRING "Graphics backend")
+set_property(CACHE SIMUTRANS_BACKEND PROPERTY STRINGS ${AVAILABLE_BACKENDS})
