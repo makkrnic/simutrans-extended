8d5e90ee51b620fdf7d771545261059da3eef076
Mak Krnic <mkrnic@gmail.com>
2020-12-09 03:26:43 +0100

    Enable GLM Experimental features for triangle normal calculation

--- sys/simsys_s2_vulkan.cc
+++ sys/simsys_s2_vulkan.cc
@@ -20,6 +20,8 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+// Needed for triangle normal calculation
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/normal.hpp>
 
 #include <sys/time.h>
